0891195f1c493e5bdcff03cff96a3a33cd82970a
Ivon Liu <ivon.liu2011@gmail.com>
2015-11-18 15:05:06 -0800

    Finish merge_licences.py

--- merge_licenses.py
+++ merge_licenses.py
@@ -6,7 +6,14 @@ Created on Tue Nov 17 22:38:55 2015
 """
 
 import pandas as pd
+    
+total_length = 0
+business_licences = []
+for i in range(1997, 2016):
+    temp = pd.read_csv("data/csv/"+str(i)+"business_licences.csv", dtype={12: str}, encoding='ISO-8859-1')
+    business_licences.append(temp)    
 
-licenses_2015 = pd.read_csv("data/csv/business_licences.csv")
+merged_licences = pd.concat(business_licences)
+merged_licences.to_csv("data/output/business_licences.csv", na_rep="", index=False)
 
-
+print("done")
