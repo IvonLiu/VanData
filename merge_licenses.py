d0c7aecc5c1f59f2ed7046e36e4221c037f5aab3
Ivon Liu <ivon.liu2011@gmail.com>
2015-11-17 22:48:55 -0800

    Initial commit

---
+++ merge_licenses.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 17 22:38:55 2015
+
+@author: Ivon Liu
+"""
+
+import pandas as pd
+
+licenses_2015 = pd.read_csv("data/csv/business_licences.csv")
+
+
